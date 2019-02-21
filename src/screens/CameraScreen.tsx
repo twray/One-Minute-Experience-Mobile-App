b44072caaca75e6170fa6cf971c34548970c79d0
Phillip Raffnsøe Nilsson <phin@itu.dk>
2019-02-21 09:17:22 +0000

    Merged PR 11: add activity indicator when snapping an image
    add activity indicator when snapping an image

--- src/screens/CameraScreen.tsx
+++ src/screens/CameraScreen.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import { View, ActivityIndicator, Text } from 'react-native';
 import { NavigationScreenProps } from 'react-navigation';
 import styles from '../styles';
 import {
@@ -8,11 +9,16 @@ import {
 import { Constants } from 'expo';
 
 import FullScreenCamera from '../components/FullScreenCamera';
+
 interface CameraScreenProps extends NavigationScreenProps {}
+
+interface CameraScreenState {
+  isLoading: boolean;
+}
 
 export default class CameraScreen extends React.Component<
   CameraScreenProps,
-  {}
+  CameraScreenState
 > {
   public static navigationOptions = {
     header: null,
@@ -21,15 +27,42 @@ export default class CameraScreen extends React.Component<
   constructor(props: CameraScreenProps) {
     super(props);
     this.handlePictureTaken = this.handlePictureTaken.bind(this);
+    this.state = {
+      isLoading: false,
+    };
   }
 
   public render() {
-    return <FullScreenCamera onPictureTaken={this.handlePictureTaken} />;
+    return (
+      <View style={{ flex: 1 }}>
+        <FullScreenCamera onPictureTaken={this.handlePictureTaken} />
+        {this.state.isLoading ? (
+          <View
+            style={{
+              position: 'absolute',
+              top: 0,
+              left: 0,
+              right: 0,
+              bottom: 0,
+              justifyContent: 'center',
+              alignItems: 'center',
+            }}
+          >
+            <ActivityIndicator size="large" />
+            {/* <Text style={{ color: 'white', marginTop: 10 }}>Fetching...</Text> */}
+          </View>
+        ) : null}
+      </View>
+    );
   }
 
   private async handlePictureTaken(imageData: string) {
+    this.setState({ isLoading: true });
     // const artwork = await getArtworkByPicture('');
     const artwork = await getArtworkById(22);
+    // fake 400ms load time
+    await new Promise(resolve => setTimeout(resolve, 400));
+    this.setState({ isLoading: false });
     this.props.navigation.navigate('StoryModal', {
       artwork,
     });
