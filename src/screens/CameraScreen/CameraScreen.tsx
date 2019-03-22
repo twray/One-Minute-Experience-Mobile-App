90f95a30124ded0f20f6d320d9006b5790e85d61
Phillip Raffnsøe Nilsson <phin@itu.dk>
2019-03-22 11:45:51 +0000

    Merged PR 50: No Internet Toast

--- src/screens/CameraScreen/CameraScreen.tsx
+++ src/screens/CameraScreen/CameraScreen.tsx
@@ -1,7 +1,12 @@
 import * as React from 'react';
-import { View, ActivityIndicator, Text, TouchableOpacity } from 'react-native';
+import {
+  View,
+  ActivityIndicator,
+  Text,
+  SafeAreaView,
+  NetInfo,
+} from 'react-native';
 import { NavigationScreenProps } from 'react-navigation';
-import { ImageManipulator } from 'expo';
 
 import FullScreenCamera from '../../components/FullScreenCamera';
 import styles from './styles';
@@ -14,6 +19,7 @@ interface CameraScreenProps extends NavigationScreenProps {}
 
 interface CameraScreenState {
   isLoading: boolean;
+  isOffline: boolean;
 }
 
 export default class CameraScreen extends React.Component<
@@ -27,10 +33,26 @@ export default class CameraScreen extends React.Component<
   constructor(props: CameraScreenProps) {
     super(props);
     this.handlePictureTaken = this.handlePictureTaken.bind(this);
+    this.handleConnectionChange = this.handleConnectionChange.bind(this);
     this.setLoading = this.setLoading.bind(this);
     this.state = {
       isLoading: false,
+      isOffline: true,
     };
+  }
+
+  public componentDidMount() {
+    NetInfo.isConnected.addEventListener(
+      'connectionChange',
+      this.handleConnectionChange,
+    );
+  }
+
+  public componentWillUnmount() {
+    NetInfo.isConnected.removeEventListener(
+      'connectionChange',
+      this.handleConnectionChange,
+    );
   }
 
   public render() {
@@ -40,6 +62,28 @@ export default class CameraScreen extends React.Component<
           setLoading={this.setLoading}
           onPictureTaken={this.handlePictureTaken}
         />
+        {this.state.isOffline && (
+          <SafeAreaView
+            style={{
+              position: 'absolute',
+              backgroundColor: '#e84118',
+              width: '100%',
+            }}
+          >
+            <Text
+              style={{
+                color: 'white',
+                textAlign: 'center',
+                padding: 10,
+                fontSize: 16,
+              }}
+            >
+              It appears like you are offline.
+              {'\n'}
+              Artworks might not be recognized.
+            </Text>
+          </SafeAreaView>
+        )}
         {this.state.isLoading ? (
           <View style={styles.loadingContainer}>
             <ActivityIndicator size="large" />
@@ -47,6 +91,12 @@ export default class CameraScreen extends React.Component<
         ) : null}
       </View>
     );
+  }
+
+  private handleConnectionChange(isConnected: boolean) {
+    this.setState({
+      isOffline: !isConnected,
+    });
   }
 
   private setLoading(value: boolean) {
