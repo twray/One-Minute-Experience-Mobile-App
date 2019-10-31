fb355258ae3d616d766b37858ab3b936c2e3ba46
Tim Wray <timwray.mail@gmail.com>
2019-10-31 10:53:01 +0100

    Updated environment variables.

--- src/environment.ts
+++ src/environment.ts
@@ -1,22 +1,27 @@
+import ENV from './env';
+
+// If you are deploying One Minute for your museum, you would need to create
+// an env.ts file that exports these values
+
 const APIEndpoints = {
-  root: 'https://modgift.itu.dk/1mev2',
-  db: 'https://modgift.itu.dk/1mev2/_'
+  root: ENV.prod.root,
+  db: ENV.prod.db
 };
 
 const ProductionKeys = {
-  endpoint: 'https://northeurope.api.cognitive.microsoft.com',
-  projectKey: '6a61c57a-8da9-469a-a5a1-de1055543a42',
-  predictionKey: 'a267e2c8185241e4808534c70f96157f',
-  iteration: 'production',
-  collection: 'artwork'
+  endpoint: ENV.prod.endpoint,
+  projectKey: ENV.prod.projectKey,
+  predictionKey: ENV.prod.predictionKey,
+  iteration: ENV.prod.iteration,
+  collection: ENV.prod.collection
 };
 
 const DevelopmentKeys = {
-  endpoint: 'https://northeurope.api.cognitive.microsoft.com',
-  projectKey: '99201fdf-3975-4922-af0d-a97f3e60158e',
-  predictionKey: 'a267e2c8185241e4808534c70f96157f',
-  iteration: 'testing',
-  collection: 'artwork_test'
+  endpoint: ENV.dev.endpoint,
+  projectKey: ENV.dev.projectKey,
+  predictionKey: ENV.dev.predictionKey,
+  iteration: ENV.dev.iteration,
+  collection: ENV.dev.collection
 }
 
 export function getAPIEndpoint() {
